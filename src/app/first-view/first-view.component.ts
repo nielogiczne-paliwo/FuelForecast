df2977f2983fc0f88a5f3ce148f84ae04c7bc7ad
Maciej Kuboń <66884064+MaciejKubon@users.noreply.github.com>
2024-05-30 22:55:49 +0200

    set view handling

--- src/app/first-view/first-view.component.ts
+++ src/app/first-view/first-view.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-first-view',
   templateUrl: './first-view.component.html',
-  styleUrls: ['./first-view.component.css']
+  styleUrls: ['./first-view.component.css'],
 })
 export class FirstViewComponent {
+  @Output() clickNextButton = new EventEmitter<number>();
 
+  nextView() {
+    this.clickNextButton.emit(2);
+  }
 }
