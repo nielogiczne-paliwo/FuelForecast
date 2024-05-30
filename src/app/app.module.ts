8e74a6e8a2ebeadd2b6b705f2f7195a6182a38fd
Maciej Kuboń <66884064+MaciejKubon@users.noreply.github.com>
2024-05-30 17:30:19 +0200

    create first view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+import { FirstViewComponent } from './first-view/first-view.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderNavbarComponent],
+  declarations: [AppComponent, HeaderNavbarComponent, FirstViewComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
