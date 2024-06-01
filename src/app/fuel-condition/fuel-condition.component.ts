add4300420ca0d3dde7d640600ccc51ea4d2d266
Maciej Kuboń <66884064+MaciejKubon@users.noreply.github.com>
2024-06-01 15:53:18 +0200

    style form, add input

--- src/app/fuel-condition/fuel-condition.component.ts
+++ src/app/fuel-condition/fuel-condition.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { stationData } from '../interface/interface';
 
 @Component({
   selector: 'app-fuel-condition',
@@ -7,16 +13,90 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./fuel-condition.component.css'],
 })
 export class FuelConditionComponent {
-  yStart = new FormControl('');
-  yStop = new FormControl('');
-  xLength = new FormControl('');
-  myForm: FormGroup = this.builder.group({
-    yStart: this.yStart,
-    yStop: this.yStop,
-    xLength: this.xLength,
-  });
+  @Input() isActive?: boolean;
+  @Output() active = new EventEmitter<boolean>();
+  fuelConditionForm: stationData;
+  anotherDate: Date = new Date(2023, 11, 25);
+  petrol: string[];
+  dataStructure: string[];
+  myForm: FormGroup = this.builder.group({});
 
-  constructor(private builder: FormBuilder) {}
+  constructor(private builder: FormBuilder) {
+    this.petrol = ['ULG95', 'DK', 'ULTSU', 'ULTDK'];
+    this.dataStructure = ['maxStock', 'capacisty', 'inputData'];
+    this.fuelConditionForm = {
+      dateStart: this.anotherDate,
+      dateEnd: this.anotherDate,
+      ULG95: {
+        maxStock: 0,
+        capacisty: 0,
+        inputData: 0,
+      },
+      DK: {
+        maxStock: 0,
+        capacisty: 0,
+        inputData: 0,
+      },
+      ULTSU: {
+        maxStock: 0,
+        capacisty: 0,
+        inputData: 0,
+      },
+      ULTDK: {
+        maxStock: 0,
+        capacisty: 0,
+        inputData: 0,
+      },
+    };
+  }
+  ngOnInit() {
+    this.myForm = this.builder.group({
+      dateStart: [null, Validators.required],
+      dateStop: [null, Validators.required],
+      ULG95maxStock: [null, Validators.required],
+      ULG95capacisty: [null, Validators.required],
+      ULG95inputData: [null, Validators.required],
+      DKmaxStock: [null, Validators.required],
+      DKcapacisty: [null, Validators.required],
+      DKinputData: [null, Validators.required],
+      ULTSUmaxStock: [null, Validators.required],
+      ULTSUcapacisty: [null, Validators.required],
+      ULTSUinputData: [null, Validators.required],
+      ULTDKmaxStock: [null, Validators.required],
+      ULTDKcapacisty: [null, Validators.required],
+      ULTDKinputData: [null, Validators.required],
+    });
+  }
 
-  onSubmit() {}
+  onSubmit() {
+    const dataForm = this.myForm.value;
+    this.fuelConditionForm = {
+      dateStart: dataForm.dateStart,
+      dateEnd: dataForm.dateStart,
+      ULG95: {
+        maxStock: dataForm.ULG95maxStock,
+        capacisty: dataForm.ULG95capacisty,
+        inputData: dataForm.ULG95inputData,
+      },
+      DK: {
+        maxStock: dataForm.DKmaxStock,
+        capacisty: dataForm.DKcapacisty,
+        inputData: dataForm.DKinputData,
+      },
+      ULTSU: {
+        maxStock: dataForm.ULTSUmaxStock,
+        capacisty: dataForm.ULTSUcapacisty,
+        inputData: dataForm.ULTSUinputData,
+      },
+      ULTDK: {
+        maxStock: dataForm.ULTDKmaxStock,
+        capacisty: dataForm.ULTDKcapacisty,
+        inputData: dataForm.ULTDKinputData,
+      },
+    };
+    this.closeForm();
+  }
+  closeForm() {
+    this.active.emit(false);
+  }
 }
--- src/app/second-view/second-view.component.ts
+++ src/app/second-view/second-view.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-second-view',
   templateUrl: './second-view.component.html',
-  styleUrls: ['./second-view.component.css']
+  styleUrls: ['./second-view.component.css'],
 })
 export class SecondViewComponent {
+  activeForm: boolean = false;
 
+  activform(value: boolean): void {
+    this.activeForm = value;
+    console.log(this.activeForm);
+  }
 }
