f5c73d8c7938aa1d15102bc1e0f2f381867be25d
JazzV2 <adam.01748.g@gmail.com>
2024-06-03 20:14:38 +0200

    Errors service for files is implemented

--- src/app/load-file-button/load-file-button.component.ts
+++ src/app/load-file-button/load-file-button.component.ts
@@ -11,9 +11,16 @@ export class LoadFileButtonComponent {
   @Input() isActive?: boolean;
   @Input() buttonText?: string;
   data: delivierData[];
-
+  isError: boolean;
+  errorText: string;
   constructor(private fileData: FileDataService){
     this.data = fileData.getData();
+    this.isError = false;
+    this.errorText = ""
+  }
+
+  activeErrorMessage(value: boolean): void {
+    this.isError = value;
   }
 
   loadData(event: any){
@@ -21,12 +28,27 @@ export class LoadFileButtonComponent {
     const fileReader = new FileReader();
     fileReader.readAsText(inputValue, 'UTF-8');
     fileReader.onload = () => {
-      const loadedData: delivierData[] = JSON.parse(fileReader.result as string).deliveries;
-      this.fileData.setData(loadedData);
+      try {
+        const loadedFile = JSON.parse(fileReader.result as string);
+        if (!("deliveries" in loadedFile) || !(loadedFile.deliveries))
+          throw "Zła zawartość pliku"
+        const loadedData: delivierData[] = loadedFile.deliveries;
+        this.fileData.setData(loadedData);
+      }catch(err) {
+        if (typeof err === "string") {
+          this.isError = true;
+          this.errorText = err;
+        } else if (err instanceof Error) {
+          this.isError = true;
+          this.errorText = "Zły format pliku, akceptowalny jest tylko json";
+        }
+      }
+      
       // this.data = this.fileData.getData()
      }
      fileReader.onerror = (error) => {
-       console.log(error);
+        this.isError = true;
+        this.errorText = `Błąd przy wczytywaniu pliku: ${error}`;
      } 
   }
 }
