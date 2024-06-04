c048d81d71ca102d6e10a948272649a5ea2ce066
Maciej Kuboń <66884064+MaciejKubon@users.noreply.github.com>
2024-06-04 19:09:52 +0200

    change error message style

--- src/app/load-file-button/load-file-button.component.ts
+++ src/app/load-file-button/load-file-button.component.ts
@@ -5,7 +5,7 @@ import { FileDataService } from '../service/file-data.service';
 @Component({
   selector: 'app-load-file-button',
   templateUrl: './load-file-button.component.html',
-  styleUrls: ['../button/button.component.css']
+  styleUrls: ['../button/button.component.css'],
 })
 export class LoadFileButtonComponent {
   @Input() isActive?: boolean;
@@ -13,42 +13,46 @@ export class LoadFileButtonComponent {
   data: delivierData[];
   isError: boolean;
   errorText: string;
-  constructor(private fileData: FileDataService){
+  errorTitle: string;
+  constructor(private fileData: FileDataService) {
     this.data = fileData.getData();
     this.isError = false;
-    this.errorText = ""
+    this.errorText = '';
+    this.errorTitle = '';
   }
 
   activeErrorMessage(value: boolean): void {
     this.isError = value;
   }
 
-  loadData(event: any){
+  loadData(event: any) {
     const inputValue = event.target.files[0];
     const fileReader = new FileReader();
     fileReader.readAsText(inputValue, 'UTF-8');
     fileReader.onload = () => {
       try {
         const loadedFile = JSON.parse(fileReader.result as string);
-        if (!("deliveries" in loadedFile) || !(loadedFile.deliveries))
-          throw "Zła zawartość pliku"
+        if (!('deliveries' in loadedFile) || !loadedFile.deliveries)
+          throw 'Zła zawartość pliku';
         const loadedData: delivierData[] = loadedFile.deliveries;
         this.fileData.setData(loadedData);
-      }catch(err) {
-        if (typeof err === "string") {
+      } catch (err) {
+        if (typeof err === 'string') {
           this.isError = true;
           this.errorText = err;
         } else if (err instanceof Error) {
           this.isError = true;
-          this.errorText = "Zły format pliku, akceptowalny jest tylko json";
+          this.errorText = 'Akceptowalny format pliku to *.json';
+          this.errorTitle = 'Zły format pliku';
         }
       }
-      
+
       // this.data = this.fileData.getData()
-     }
-     fileReader.onerror = (error) => {
-        this.isError = true;
-        this.errorText = `Błąd przy wczytywaniu pliku: ${error}`;
-     } 
+    };
+    fileReader.onerror = (error) => {
+      this.isError = true;
+      this.errorText = `Błąd przy wczytywaniu pliku: ${error}`;
+      this.errorTitle = 'Błąd';
+    };
   }
 }
