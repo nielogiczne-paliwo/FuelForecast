d2f5a45ffdd53d75b86a3b38948ea7d9f9985f6e
Maciej Kuboń <66884064+MaciejKubon@users.noreply.github.com>
2024-06-07 23:57:59 +0200

    change service structure, add dashboard service and add session service

--- src/app/dashboard-view/dashboard-view.component.ts
+++ src/app/dashboard-view/dashboard-view.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { DashboardViewService } from '../service/dashboard-view.service';
+import { delivierData } from '../interface/interface';
 
 @Component({
   selector: 'app-dashboard-view',
   templateUrl: './dashboard-view.component.html',
-  styleUrls: ['./dashboard-view.component.css']
+  styleUrls: ['./dashboard-view.component.css'],
 })
 export class DashboardViewComponent {
-
+  constructor(DashboardService: DashboardViewService) {
+    DashboardService.getSumDeliveryFuel();
+  }
 }
